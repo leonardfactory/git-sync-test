b2f81429e21884ab31119775fa72f106aa3ba906
Leonardo Ascione <leonardo.ascione@gmail.com>
2013-02-24 01:30:17 +0100

    Added base model

---
+++ GitSyncTest/LFProjection.m
@@ -0,0 +1,13 @@
+//
+//  LFProjection.m
+//  GitSyncTest
+//
+//  Created by Leonardo Ascione on 24/02/13.
+//  Copyright (c) 2013 LeonardFactory. All rights reserved.
+//
+
+#import "LFProjection.h"
+
+@implementation LFProjection
+
+@end
