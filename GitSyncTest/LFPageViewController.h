fc21768e22675fa4a87f5bcedbfee7ea5a76af26
Leonardo Ascione <leonardo.ascione@gmail.com>
2013-02-24 01:22:54 +0100

    Added controllers

---
+++ GitSyncTest/LFPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  LFPageViewController.h
+//  GitSyncTest
+//
+//  Created by Leonardo Ascione on 24/02/13.
+//  Copyright (c) 2013 LeonardFactory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LFPageViewController : UIViewController
+
+@end
---
+++ GitSyncTest/LFPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  LFPageViewController.m
+//  GitSyncTest
+//
+//  Created by Leonardo Ascione on 24/02/13.
+//  Copyright (c) 2013 LeonardFactory. All rights reserved.
+//
+
+#import "LFPageViewController.h"
+
+@interface LFPageViewController ()
+
+@end
+
+@implementation LFPageViewController
+
+- (LFPageViewController *)initWithCoder:(NSCoder *) coder
+{
+	if(self = [super init])
+	{
+		return self;
+	}
+	return nil;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
